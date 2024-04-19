a4f223d8a3527d0a4de6b4c12157f99ec3280a2b
kawzaiphophu <kawzaiphophu@gmail.com>
2024-04-19 23:24:24 +0700

    fix cart delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Shopee from './page/Shopee';
 import ShopeeDetail from './page/ShopeeDetail';
